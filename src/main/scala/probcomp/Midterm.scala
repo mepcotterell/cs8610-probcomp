56bb8aea0b4f848f7e253e666c8ed3b2bdf101c1
Michael Cotterell <mepcotterell@gmail.com>
2012-09-26 12:58:19 -0400

    updated Midterm.scala

--- src/main/scala/probcomp/Midterm.scala
+++ src/main/scala/probcomp/Midterm.scala
@@ -1,7 +1,30 @@
 package probcomp
 
+import scala.collection.mutable.Set
+
+class DisjointSetList (n: Int) {
+
+  // create an array of mutable sets
+  val sets = Array.ofDim[Set[Int]](n)
+  
+  // create the sets
+  for (i <- 0 until sets.length) sets(i) = Set()
+
+  def find (i: Int): Int = {
+    for (e <- sets.view.zipWithIndex if e._1.exists(_ == i)) return e._2
+    return -1
+  } // find
+
+  def untion (i: Int, j: Int) = {}
+
+} // DisjointSetList
+
 object Midterm extends App {
 
+
+  val dsl = new DisjointSetList(10)
+
+  dsl.find(2)
 
 } // Midterm
 
