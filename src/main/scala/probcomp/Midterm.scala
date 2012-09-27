113d74480db3c8c3d76d9c79698c83260c82e124
Michael Cotterell <mepcotterell@gmail.com>
2012-09-27 14:24:21 -0400

    added comments

--- src/main/scala/probcomp/Midterm.scala
+++ src/main/scala/probcomp/Midterm.scala
@@ -1,8 +1,15 @@
 package probcomp
 
-import scala.collection.mutable.Set
-
+/**
+ * A disjoint sets data structure.
+ *
+ * @author Michael E. Cotterell <mepcotterell@gmail.com>
+ * @param n the number of initial disjoint sets
+ */
 class DisjointSets (n: Int) {
+
+  // make sure we're using mutable sets
+  import scala.collection.mutable.Set
 
   // create an array of mutable sets
   val sets = Array.ofDim[Set[Int]](n)
@@ -10,8 +17,21 @@ class DisjointSets (n: Int) {
   // create the sets
   for (i <- 0 until sets.length) sets(i) = Set(i)
 
+  /**
+   * Returns the indices in the set array of the sets that contain x.
+   *
+   * @param an element to find
+   * @return an array of indices corresponding to the sets containing x
+   */
   def find (x: Int): Array[Int] = sets.view.zipWithIndex.filter(e => e._1.exists(_ == x)).map(e => e._2).toArray
 
+  /**
+   * Unites the dynamic sets that contains x and y into a new set that is the
+   * union of these two sets.
+   *
+   * @param x an element of the first set
+   * @param y an element of the second set
+   */
   def union (x: Int, y: Int): Unit = for (xRoot <- find(x); yRoot <- find(y)) {
     sets(xRoot) ++= sets(yRoot)
     sets(yRoot)   = sets(xRoot)
@@ -19,8 +39,20 @@ class DisjointSets (n: Int) {
 
 } // DisjointSets
 
+/**
+ * A disjoint sets datastructure made to look more like a graph.
+ *
+ * @author Michael E. Cotterell <mepcotterell@gmail.com>
+ * @param n the number of initial disjoint sets
+ */
 class DisjointSetGraph (n: Int) extends DisjointSets (n) {
 
+  /**
+   * Adds an edge to the graph.
+   *
+   * @param x one vertex in the edge
+   * @param y the other vertex in the edge
+   */
   def addEdge (x: Int, y: Int): Unit = {
     union(x, y)
     println("add (%d, %d) => %s".format(x, y, sets.deep))
@@ -28,6 +60,9 @@ class DisjointSetGraph (n: Int) extends DisjointSets (n) {
 
 } // DisjointSetGraph
 
+/**
+ * Code for the midterm assignment
+ */
 object Midterm extends App {
 
   val dsg = new DisjointSetGraph(10)
