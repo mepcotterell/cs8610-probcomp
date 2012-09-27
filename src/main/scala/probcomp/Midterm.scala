39d351a0bda7ac30158d7c09db1594d366a7b0fc
Michael Cotterell <mepcotterell@gmail.com>
2012-09-27 14:17:10 -0400

    updated the disjoint sets code

--- src/main/scala/probcomp/Midterm.scala
+++ src/main/scala/probcomp/Midterm.scala
@@ -2,29 +2,45 @@ package probcomp
 
 import scala.collection.mutable.Set
 
-class DisjointSetList (n: Int) {
+class DisjointSets (n: Int) {
 
   // create an array of mutable sets
   val sets = Array.ofDim[Set[Int]](n)
   
   // create the sets
-  for (i <- 0 until sets.length) sets(i) = Set()
+  for (i <- 0 until sets.length) sets(i) = Set(i)
 
-  def find (i: Int): Int = {
-    for (e <- sets.view.zipWithIndex if e._1.exists(_ == i)) return e._2
-    return -1
-  } // find
+  def find (x: Int): Array[Int] = sets.view.zipWithIndex.filter(e => e._1.exists(_ == x)).map(e => e._2).toArray
 
-  def untion (i: Int, j: Int) = {}
+  def union (x: Int, y: Int): Unit = for (xRoot <- find(x); yRoot <- find(y)) {
+    sets(xRoot) ++= sets(yRoot)
+    sets(yRoot)   = sets(xRoot)
+  } // union
 
-} // DisjointSetList
+} // DisjointSets
+
+class DisjointSetGraph (n: Int) extends DisjointSets (n) {
+
+  def addEdge (x: Int, y: Int): Unit = {
+    union(x, y)
+    println("add (%d, %d) => %s".format(x, y, sets.deep))
+  } // addEdge
+
+} // DisjointSetGraph
 
 object Midterm extends App {
 
+  val dsg = new DisjointSetGraph(10)
 
-  val dsl = new DisjointSetList(10)
+  println("initial => %s".format(dsg.sets.deep))
 
-  dsl.find(2)
+  dsg.addEdge(1, 3)
+  dsg.addEdge(4, 6)
+  dsg.addEdge(0, 2)
+  dsg.addEdge(7, 8)
+  dsg.addEdge(0, 1)
+  dsg.addEdge(4, 5)
+  dsg.addEdge(1, 2)
 
 } // Midterm
 
