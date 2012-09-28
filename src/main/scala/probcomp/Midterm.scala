be65c4a79f13f8a3c1bc46fb5cdae9ef3c288402
Michael Cotterell <mepcotterell@gmail.com>
2012-09-27 21:00:35 -0400

    completed the code for the experiment

--- src/main/scala/probcomp/Midterm.scala
+++ src/main/scala/probcomp/Midterm.scala
@@ -11,20 +11,25 @@ class DisjointSets (n: Int) {
   // make sure we're using mutable sets
   import scala.collection.mutable.Set
 
-  // create a parallel array of mutable sets
-  val sets = Array.ofDim[Set[Int]](n).par
-  
-  // create the sets
-  for (i <- 0 until sets.length) sets(i) = Set(i)
+  // create a parallel set of mutable sets
+  val sets = Set.empty[Set[Int]]
 
+  // create the initial disjoint sets
+  for (i <- 0 until n) {
+    sets add Set(i)
+  } // for
+ 
   /**
    * Returns the indices in the set array of the sets that contain x.
-   * Use a view to make the zip and filter fast.
    * 
    * @param an element to find
    * @return an array of indices corresponding to the sets containing x
    */
-  def find (x: Int): Array[Int] = sets.view.zipWithIndex.filter(e => e._1.exists(_ == x)).map(e => e._2).toArray
+  def find (x: Int): Set[Int] = {
+    var set: Set[Int] = null
+    sets.view.filter(set => set.exists(_ == x)).foreach(s => set = s)
+    set
+  } // find
 
   /**
    * Unites the dynamic sets that contains x and y into a new set that is the
@@ -33,9 +38,18 @@ class DisjointSets (n: Int) {
    * @param x an element of the first set
    * @param y an element of the second set
    */
-  def union (x: Int, y: Int): Unit = for (xRoot <- find(x); yRoot <- find(y)) {
-    sets(xRoot) ++= sets(yRoot)
-    sets(yRoot)   = sets(xRoot)
+  def union (x: Int, y: Int): Unit = {
+    val xSet = find(x)
+    val ySet = find(y)
+    if (xSet != ySet){
+      if (xSet.size > ySet.size) {
+	xSet ++= ySet
+	sets remove ySet
+      } else {
+	ySet ++= xSet
+	sets remove xSet
+      } // if
+    } // if
   } // union
 
 } // DisjointSets
@@ -57,34 +71,116 @@ class DisjointSetGraph (n: Int) extends DisjointSets (n) {
    * @param x one vertex in the edge
    * @param y the other vertex in the edge
    */
-  def addEdge (x: Int, y: Int): Unit = {
-    union(x, y)
-  //  println("add (%d, %d) => %s".format(x, y, sets.deep))
-  //  println("number of connected components = %d".format(numComponents))
-  } // addEdge
+  def addEdge (x: Int, y: Int): Unit = union(x, y)
 
   def numComponents: Int = sets.toSet.size
 
 } // DisjointSetGraph
+
+object Hypergeometric {
+
+  /**
+   * Regularized Incomplete Beta Function
+   */
+  //def I (x: Int, a: Double, b: Double) = B(x, a ,b) / B(a, b)
+
+} // Hypergeometric
+
+/**
+ * Statistic class based on code from the ScalaTion project
+ */
+class Statistic () {
+
+  import scala.math.sqrt
+
+  // samples
+  protected var n = 0
+
+  // sum
+  protected var sum = 0.
+  
+  // sum of squares
+  protected var sumSq = 0.
+
+  // min
+  protected var minX = Double.MaxValue
+
+  // max
+  protected var maxX = 0.
+
+  /**
+   * Samples a value
+   *
+   * @param x the value sampled
+   */
+  def sample (x: Double) {
+    n     += 1
+    sum   += x
+    sumSq += x * x
+    if (x < minX) minX = x
+    if (x > maxX) maxX = x
+  } // sample
+
+  def size     = n
+  def min      = if (n == 0) 0. else minX
+  def max      = if (n == 0) 0. else maxX
+  def mean     = if (n == 0) 0. else sum / n.toDouble
+  def variance = if (n == 0) 0. else sumSq / (n.toDouble - 1) - mean * mean
+  def stddev   = sqrt(variance)
+  def ms       = sumSq / n.toDouble 
+  def rms      = sqrt(ms)
+
+  def printSummary: Unit = {
+    println("| %7s | %9s | %9s | %9s | %9s |".format("samples", "min", "max", "mean", "stdDev"))
+    println("| %7d | %9.3f | %9.3f | %9.3f | %9.3f |".format(n, min, max, mean, stddev))
+  } // printSummary
+
+} // Statistic
 
 /**
  * Code for the midterm assignment
  */
 object Midterm extends App {
 
-  val dsg = new DisjointSetGraph(10)
+  import scala.util.control.Breaks._
+  import scala.util.Random
 
-//  println("initial => %s".format(dsg.sets.deep))
+  // 100(100)1000
+  for (n <- 100 to 1000 by 100) {
+    
+    val stat = new Statistic()
+    val r    = new Random()
+    val p    = 0.5
 
-  dsg.addEdge(1, 3)
-  dsg.addEdge(4, 6)
-  dsg.addEdge(0, 2)
-  dsg.addEdge(7, 8)
-  dsg.addEdge(0, 1)
-  dsg.addEdge(4, 5)
-  dsg.addEdge(1, 2)
+    var g: DisjointSetGraph = null
 
-  println("number of connected components = %d".format(dsg.numComponents))
+    // need to take 100 samples
+    for (i <- 0 until 100) {
+
+      var counter = 0
+      
+      g = new DisjointSetGraph(n)
+
+      breakable {
+	for (edge <- (0 until n).combinations(2).toList) {
+	  if (r.nextDouble < p) {
+	    g.addEdge(edge(0), edge(1))
+	    counter += 1
+	  } // if
+          if (g.numComponents == 1) break
+	} // for
+      } // breakable
+      
+      // add our sample to the statistic
+      stat.sample(counter)
+
+    } // for
+
+    println
+    println("n = %d".format(n))
+    stat.printSummary
+
+  } // for 
 
 } // Midterm
 
